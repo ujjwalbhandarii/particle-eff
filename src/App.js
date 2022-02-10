e85991bdf699f81f787bc5fdd601ffceea877304
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-02-10 20:38:12 +0545

    sarkar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import ParticleBackground from "./ParticleBackground";
+
+function App() {
+  return (
+    <div>
+      <ParticleBackground />
+      <div className="container">
+        <h1>Sachin sarkar</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
